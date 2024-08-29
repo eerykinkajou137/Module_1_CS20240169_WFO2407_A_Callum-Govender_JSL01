94ff8ec4db846335d4188c254b4fc1292ad0f2da
Callum Govender <callumlevigovender@gmail.com>
2024-08-29 15:48:19 +0200

    String Validation

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-
+        if( input.startsWith('pet_')){
+            result ='Valid Syntax.'; 
+        }
+        else{
+            result ='Invalid Syntax.'; 
+        }
+        
             document.getElementById('result').innerText = result;
 }
 
